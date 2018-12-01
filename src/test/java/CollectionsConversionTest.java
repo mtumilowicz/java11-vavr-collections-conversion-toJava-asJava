17ae64c9c7113eaa6d23cdb66362d378693d99cd
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-02 00:25:19 +0100

    tests

--- src/test/java/CollectionsConversionTest.java
+++ src/test/java/CollectionsConversionTest.java
@@ -12,10 +12,26 @@ public class CollectionsConversionTest {
     
     @Test
     public void vavrList_toJavaList() {
+        java.util.List<Integer> integers = List.of(1).toJavaList();
+        
+        integers.add(2);
+        
+        assertThat(integers, hasSize(2));
+        assertThat(integers.get(0), is(1));
+        assertThat(integers.get(1), is(2));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void vavrList_asJava_immutable() {
         java.util.List<Integer> integers = List.of(1).asJava();
-        
-        integers.add(1);
-        
+
+        integers.add(2);
+    }
+
+    @Test
+    public void vavrList_asJava() {
+        java.util.List<Integer> integers = List.of(1).asJava();
+
         assertThat(integers, hasSize(1));
         assertThat(integers.get(0), is(1));
     }
